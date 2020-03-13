5feac3b03b5142604a01c13d82093e47960fcc6a
Cru1seControl <cru1secontrol.loot@gmail.com>
2020-03-12 21:31:09 -0400

    cordinate, rating, photo, openNow, address, name Added
    placesParsed is now json. Note photo has a downloader, could need updating in future for multiple images.

--- googleapi/places.py
+++ googleapi/places.py
@@ -10,7 +10,7 @@ class places(object):
         placeRequest = instance.request("GET", f"https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input={quotedParams}&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key={auth}")
         self._placesInfo = placeRequest.data
 
-    def placesParsed(self, formatting=False):
+    def json(self, formatting=False):
         placesCleaned = json.loads(self._placesInfo.decode())["candidates"][0]
         placesDumped = json.dumps(placesCleaned)
 
@@ -48,11 +48,93 @@ class places(object):
         placesLat = json.loads(placesLoDumped)["lat"]
         placesLng = json.loads(placesLoDumped)["lng"]
 
-        parsed = {"name": placesName, "open": openNow, "rating": ratingScore, "address": formattedAddress.split(", "), "contrib_url": urlReplace, "contrib_lat": placesLat, "contrib_lng": placesLng}
+        parsed = {"name": placesName,
+                  "open": openNow,
+                  "rating": ratingScore,
+                  "address": formattedAddress.split(", "),
+                  "contrib_url": urlReplace,
+                  "contrib_lat": placesLat,
+                  "contrib_lng": placesLng}
 
         if formatting is False:
             return parsed
         return json.dumps(parsed, indent=4)
+
+    def name(self):
+        nameCleaned = json.loads(self._placesInfo.decode())["candidates"][0]
+        nameDumped = json.dumps(nameCleaned)
+
+        placeName = json.loads(nameDumped)["name"]
+        return placeName
+
+    def address(self):
+        addressCleaned = json.loads(self._placesInfo.decode())["candidates"][0]
+        addressDumped = json.dumps(addressCleaned)
+
+        addressList = json.loads(addressDumped)["formatted_address"].split(", ")
+        return addressList
+
+    def openNow(self):
+        openingCleaned = json.loads(self._placesInfo.decode())["candidates"][0]
+        openingDumped = json.dumps(openingCleaned)
+        if "opening_hours" in openingCleaned:
+            openingHours = json.loads(openingDumped)["opening_hours"]
+            openingHoursDumped = json.dumps(openingHours)
+
+            isOpen = json.loads(openingHoursDumped)["open_now"]
+            return isOpen
+
+        else:
+            isOpen = None
+
+    def photo(self, auth, download=False):
+        photosInstance = urllib3.PoolManager()
+
+        photosCleaned = json.loads(self._placesInfo.decode())["candidates"][0]
+        photosDumped = json.dumps(photosCleaned)
+
+        photosJson = json.loads(photosDumped)["photos"][0]
+        photosJsonDumped = json.dumps(photosJson)
+
+        photosReference = json.loads(photosJsonDumped)["photo_reference"]
+        
+        photosWidth = json.loads(photosJsonDumped)["width"]
+        photosUrl = f"https://maps.googleapis.com/maps/api/place/photo?maxwidth={photosWidth}&photoreference={photosReference}&key={auth}"
+
+        if download is True:
+            photosRequest = photosInstance.request("GET", photosUrl)
+
+            with open("photoreference.jpg", "wb") as download_image:
+                download_image.write(photosRequest.data)
+        
+        return photosUrl
+
+    def rating(self):
+        ratingCleaned = json.loads(self._placesInfo.decode())["candidates"][0]
+        ratingDumped = json.dumps(ratingCleaned)
+
+        if "rating" in ratingDumped:
+            rating = json.loads(ratingDumped)["rating"]
+
+            return float(rating)
+
+        else:
+            rating = None
+
+    def cordinate(self):
+        cordinateCleaned = json.loads(self._placesInfo.decode())["candidates"][0]
+        cordinateDumped = json.dumps(cordinateCleaned)
+    
+        geo = json.loads(cordinateDumped)["geometry"]
+        geoDumped = json.dumps(geo)
+
+        cords = json.loads(geoDumped)["location"]
+        cordsDumped = json.dumps(cords)
+
+        latitude = json.loads(cordsDumped)["lat"]
+        longitude = json.loads(cordsDumped)["lng"]
+
+        return (latitude, longitude)
 
     def status(self):
         placesStatus = json.loads(self._placesInfo.decode())["status"]
